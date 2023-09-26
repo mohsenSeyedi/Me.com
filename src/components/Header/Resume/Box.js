256a0ea16a89166ebbf35c29484341393d920325
mohsen <mohsenseyedi1382@gmail.com>
2023-09-26 17:03:01 +0330

    add fileServices to components

--- src/components/Header/Resume/Box.js
+++ src/components/Header/Resume/Box.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import "./Box.css"
 
-export default function Box() {
+export default function Box({title , year}) {
   return (
     <div className="box">
-        <span>2014-2015</span>
-        <h4>Master Degree of Design</h4>
+        <span>{year}</span>
+        <h4>{title}</h4>
         <h6>CAMBRIDGE UNIVERSITY</h6>
         <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
     </div>
--- src/components/Header/Resume/Resume.js
+++ src/components/Header/Resume/Resume.js
@@ -11,12 +11,12 @@ export default function Resume() {
                 <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
             </div>
             <div className="resume-boxes">
-              <Box></Box>
-              <Box></Box>
-              <Box></Box>
-              <Box></Box>
-              <Box></Box>
-              <Box></Box>
+              <Box year="2014-2015" title="Master Degree of Design"></Box>
+              <Box year="2014-2015" title="Art & Creative Director"></Box>
+              <Box year="2017-2018" title="Bachelor's Degree of C.A"></Box>
+              <Box year="2019-2020" title="UI/UX Designer"></Box>
+              <Box year="2014-2015" title="Wordpress Developer"></Box>
+              <Box year="2022-2023" title="Diploma in Computer"></Box>
             </div>
             <div className="resume-btn">
                 <a href="#">Download cv</a>
