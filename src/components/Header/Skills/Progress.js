15c62c34432d12336ddd88306894674851ec11ba
mohsen <mohsenseyedi1382@gmail.com>
2023-09-27 13:39:38 +0330

    update code Skills component

--- src/components/Header/Skills/Progress.js
+++ src/components/Header/Skills/Progress.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import "./Progress.css"
 
-export default function Progress() {
+export default function Progress({title , percent , width}) {
   return (
-    <div>
-      
+    <div className="animate-box">
+        <div className="animate-box-text">
+          <span>{title}</span>
+          <span>{percent}</span>
+        </div>
+        <div className="progress">
+          <div className="progress-bar" style={{width: `${width}`}}></div>
+        </div>
     </div>
   )
 }
--- src/components/Header/Skills/Skills.js
+++ src/components/Header/Skills/Skills.js
@@ -4,8 +4,21 @@ import Progress from './Progress'
 
 export default function Skills() {
   return (
-    <div>
-      <Progress/>
+    <div className="skills">
+    <div className="skills-container">
+        <div className="skills-head">
+            <h4 className="section-text-head">My skills</h4>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+        </div>
+        <div className="skills-boxes">
+          <Progress width={"50%"} title="Photoshop" percent="50%"></Progress>
+          <Progress width={"75%"} title="js" percent="75%"></Progress>
+          <Progress width={"92%"} title="HTML" percent="92%"></Progress>
+          <Progress width={"90%"} title="CSS" percent="90%"></Progress>
+          <Progress width={"80%"} title="Tailwind" percent="80%"></Progress>
+          <Progress width={"30%"} title="React" percent="30%"></Progress>
+        </div>
     </div>
+</div>
   )
 }
