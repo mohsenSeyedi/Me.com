18d1f32af00a780d856506149c26fdfd1f4b26ff
mohsen <mohsenseyedi1382@gmail.com>
2023-09-28 15:24:20 +0330

    The code to change the Resume component file from css to tailwind

--- src/components/Header/Resume/Box.js
+++ src/components/Header/Resume/Box.js
@@ -3,11 +3,11 @@ import "./Box.css"
 
 export default function Box({title , year}) {
   return (
-    <div className="box">
-        <span>{year}</span>
-        <h4>{title}</h4>
-        <h6>CAMBRIDGE UNIVERSITY</h6>
-        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+    <div className="p-12 text-5xl bg-[#1a1a1a] rounded-2xl flex flex-col justify-around">
+        <span className="font-bold text-yellow-color">{year}</span>
+        <h4 className="text-5xl pt-6">{title}</h4>
+        <h6 className="text-gray-color font-bold text-2xl pt-6">CAMBRIDGE UNIVERSITY</h6>
+        <p className="text-gray-color text-[1.7rem] pt-10 leading-9">A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
     </div>
   )
 }
--- src/components/Header/Resume/Resume.js
+++ src/components/Header/Resume/Resume.js
@@ -4,13 +4,13 @@ import Box from "./Box";
 
 export default function Resume() {
   return (
-    <div className="resume">
-        <div className="resume-container">
-            <div className="resume-head">
-                <h4 className="section-text-head">Resume</h4>
-                <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+    <div id="resume" className="w-full flex items-center justify-center mt40">
+        <div id="resume-container" className="w-[95%] flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center">
+                <h4 className="header-text before:content-['Resume'] before:top-[-2rem]">Resume</h4>
+                <p className="w-[90%] text-center text-3xl mt-14">A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
             </div>
-            <div className="resume-boxes">
+            <div id="resume-boxes" className="grid grid-cols-2 gap-12 mt-24">
               <Box year="2014-2015" title="Master Degree of Design"></Box>
               <Box year="2014-2015" title="Art & Creative Director"></Box>
               <Box year="2017-2018" title="Bachelor's Degree of C.A"></Box>
@@ -18,8 +18,8 @@ export default function Resume() {
               <Box year="2014-2015" title="Wordpress Developer"></Box>
               <Box year="2022-2023" title="Diploma in Computer"></Box>
             </div>
-            <div className="resume-btn">
-                <a href="#">Download cv</a>
+            <div className="mt-32">
+                <a href="#" className="btn py-8 px-24 text-3xl">Download cv</a>
             </div>
         </div>
     </div>
