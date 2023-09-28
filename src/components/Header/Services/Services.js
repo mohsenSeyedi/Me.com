d27760e50f82864a60a3fe81a6e3c0091b61ae7f
mohsen <mohsenseyedi1382@gmail.com>
2023-09-28 16:25:02 +0330

    The code to change the Services component file from css to tailwind

--- src/components/Header/Services/Services.js
+++ src/components/Header/Services/Services.js
@@ -1,22 +1,21 @@
 import React from 'react'
-import "./Services.css"
 import Box from './Box'
 
 export default function Services() {
   return (
-    <div className="services">
-        <div className="services-container">
-            <div className="services-head">
-                <h4 className="section-text-head">services</h4>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+    <div id="services" className="w-full mt-40 flex items-center justify-center">
+        <div className="w-[95%] flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center">
+                <h4 className="header-text before:content-['services'] before:mt-[-2rem] before:left-[-6rem]">services</h4>
+                <p className="text-3xl mt-14">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
             </div>
-            <div className="services-boxes">
-              <Box icon="dfsdf" title="Web Design"></Box>
-              <Box icon="isadj" title="Phtography"></Box>
-              <Box icon="dfsdf" title="Product Strategy"></Box>
-              <Box icon="isadj" title="Web Design"></Box>
-              <Box icon="dfsdf" title="Phtography"></Box>
-              <Box icon="isadj" title="Product Strategy"></Box>
+            <div className="grid grid-cols-3 gap-12 mt-20">
+              <Box title="Web Design"></Box>
+              <Box title="Phtography"></Box>
+              <Box title="Product Strategy"></Box>
+              <Box title="Web Design"></Box>
+              <Box title="Phtography"></Box>
+              <Box title="Product Strategy"></Box>
             </div>
         </div>
     </div>
--- src/components/Header/Services/box.js
+++ src/components/Header/Services/box.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import "./Box.css"
+import { AiFillAlert } from 'react-icons/ai';
 
-export default function Box({title , icon}) {
+export default function Box({title}) {
   return (
-    <div className="box-services">
-        <span className="icon">
-            <i>{icon}</i>
-        </span>
+    <div id="box-services" className="w-[28rem] h-[20rem] p-12 text-5xl bg-[#1a1a1a] rounded-sm flex flex-col items-center justify-evenly text-center transition-all duration-300 hover:bg-yellow-color">
+        <div className="icon">
+            <AiFillAlert/>
+        </div>
         <div className="desc">
-            <h4>{title}</h4>
-            <hr/>
+            <h4 className="text-2xl uppercase">{title}</h4>
+            <hr className="w-[30%] bg-yellow-color text-center mt-8 mx-auto"/>
         </div>
     </div>
   )
