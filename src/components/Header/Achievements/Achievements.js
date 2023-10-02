6c6062e7dc4fdd222cd09341dbc01628990f144f
mohsen <mohsenseyedi1382@gmail.com>
2023-10-02 20:14:52 +0330

    refactor Achievements file to components

--- src/components/Header/Achievements/Achievements.js
+++ src/components/Header/Achievements/Achievements.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import Box from './Box'
 import "./Achievements.css"
 
 export default function Achievements() {
+
+  const [achievements ,setAchievements] = useState([
+    {id:1 , title:"Awards" , number:"100"},
+    {id:2 , title:"Complete Projects" , number:"1,200"},
+    {id:3 , title:"Happy Customers" , number:"1,200"},
+    {id:4 , title:"Cups of coffee" , number:"500"},
+  ])
+
   return (
     <div id="achievements" className="w-full h-auto flex flex-col items-center justify-center relative mt-32">
         <div id="achievements-boxes" className="w-11/12 grid grid-cols-4 gap-12 absolute top-36">
-            <Box number={"100"} title={"Awards"}/>
-            <Box number={"1,200"} title={"Complete Projects"}/>
-            <Box number={"1,200"} title={"Happy Customers"}/>
-            <Box number={"500"} title={"Cups of coffee"}/>
+          {achievements.map(achievement => (
+            <Box key={achievement.id} {...achievement}/>
+          ))}
         </div>
         <div id="container" className="w-full h-[600px] flex items-center justify-center mt-64">
             <div className="w-9/12 flex flex-col items-center justify-center text-center text-3xl mt-32">
