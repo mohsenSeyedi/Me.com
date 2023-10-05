f8ee5deed683d90d5608f9bede312cd71bb8d0f2
mohsen <mohsenseyedi1382@gmail.com>
2023-10-05 14:40:24 +0330

    add data.js file to Skills component

--- src/components/Header/Skills/Progress.js
+++ src/components/Header/Skills/Progress.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 
-export default function Progress({title , percent , width}) {
+export default function Progress(props) {
   return (
     <div className="w-full flex flex-col">
         <div className="w-[90%] flex items-center justify-between text-[2rem]">
-          <span>{title}</span>
-          <span>{percent}</span>
+          <span>{props.title}</span>
+          <span>{props.percent}</span>
         </div>
         <div className="w-full h-3 rounded-lg bg-[#1a1a1a] mt-4">
-          <div className="h-full rounded-lg bg-yellow-color" style={{width: `${width}`}}></div>
+          <div className="h-full rounded-lg bg-yellow-color" style={{width: `${props.percent}`}}></div>
         </div>
     </div>
   )
--- src/components/Header/Skills/Skills.js
+++ src/components/Header/Skills/Skills.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Progress from './Progress'
+import skillsData from "./data"
 
 export default function Skills() {
+
+  const [skills , setSkills] = useState(skillsData)
+  
   return (
     <div id="skills" className="w-full mt-40 flex items-center justify-center">
     <div className="w-[95%] flex flex-col items-center justify-center">
@@ -10,12 +14,9 @@ export default function Skills() {
             <p className="text-3xl text-center mt-14">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
         </div>
         <div className="w-[95%] grid grid-cols-2 gap-12 mt-20">
-          <Progress width={"50%"} title="Photoshop" percent="50%"></Progress>
-          <Progress width={"75%"} title="js" percent="75%"></Progress>
-          <Progress width={"92%"} title="HTML" percent="92%"></Progress>
-          <Progress width={"90%"} title="CSS" percent="90%"></Progress>
-          <Progress width={"80%"} title="Tailwind" percent="80%"></Progress>
-          <Progress width={"30%"} title="React" percent="30%"></Progress>
+          {skills.map(skill => (
+            <Progress key={skill.id} {...skill}></Progress>
+          ))}
         </div>
     </div>
 </div>
