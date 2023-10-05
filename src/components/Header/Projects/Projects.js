3032fae15199164a41ee555ec5efae0c742cf4b1
mohsen <mohsenseyedi1382@gmail.com>
2023-10-05 15:17:03 +0330

    add data.js file to Projects component

--- src/components/Header/Projects/Projects.js
+++ src/components/Header/Projects/Projects.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
 import "./Projects.css"
+import 'swiper/css/navigation';
+import projectData from './data';
 import Project from './Project'
 
 export default function Projects() {
     
+    const [projects , setProjecs] = useState(projectData)
+
   return (
     <div id="projects-section" className="w-full flex items-center justify-center mt-60">
         <div className="w-[95%] flex flex-col items-center justify-center">
@@ -11,16 +18,11 @@ export default function Projects() {
                 <h4 className="header-text before:content-['Projects'] before:mt-[-1rem] before:left-[-3rem]">Our Projects</h4>
                 <p className="text-3xl mt-14">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
             </div>
-            <div className="w-full flex items-center justify-center mt-20">
-                <div className="w-[95%] h-auto grid grid-cols-3 grid-rows-3 gap-12">
-                    <Project id={"project-1"} classContainer={"col-span-1"} title={"Branding & Illustration Design"} subTitle={"Web Design"}/>
-                    <Project id={"project-2"} classContainer={"col-span-2"} title={"Branding & Illustration Design"} subTitle={"Web Design"}/>
-                    <Project id={"project-3"} classContainer={"col-span-2"} title={"Branding & Illustration Design"} subTitle={"Web Design"}/>
-                    <Project id={"project-4"} classContainer={"col-span-1"} title={"Branding & Illustration Design"} subTitle={"Web Design"}/>
-                    <Project id={"project-5"} classContainer={"col-span-1"} title={"Branding & Illustration Design"} subTitle={"Web Design"}/>
-                    <Project id={"project-6"} classContainer={"col-span-2"} title={"Branding & Illustration Design"} subTitle={"Web Design"}/>
-                </div>
-            </div>
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper w-full h-full">
+                {projects.map(project => (
+                    <SwiperSlide key={project.id} {...project} className=""><Project/></SwiperSlide>
+                ))}
+            </Swiper>
         </div>
     </div>
   )
---
+++ src/components/Header/Projects/data.js
@@ -0,0 +1,37 @@
+
+const projectData = [
+    {
+        id: 1 ,
+        title: 'SiS',
+        description: ' Buy all kinds of SiS shoes from our site and enjoy it 😉 I really enjoy the Ui of this site',
+        type: 'Web Design',
+        img: './img/projects/project-1.jpg.webp',
+        tag: ['Api', 'React','Tailwind']
+    },
+    {
+        id: 2 ,
+        title: 'SiS',
+        description: ' Buy all kinds of SiS shoes from our site and enjoy it 😉 I really enjoy the Ui of this site',
+        type: 'Web Design',
+        img: './img/projects/project-1.jpg.webp',
+        tag: ['Api', 'React','Tailwind']
+    },
+    {
+        id: 3 ,
+        title: 'SiS',
+        description: ' Buy all kinds of Nike shoes from our site and enjoy it 😉 I really enjoy the Ui of this site',
+        type: 'Web Design',
+        img: './img/projects/project-1.jpg.webp',
+        tag: ['Api', 'React','Tailwind',"TypeScript"]
+    },
+    {
+        id: 4 ,
+        title: 'SiS',
+        description: ' Buy all kinds of SiS shoes from our site and enjoy it 😉 I really enjoy the Ui of this site',
+        type: 'Web Design',
+        img: './img/projects/project-1.jpg.webp',
+        tag: ['Api', 'React','Tailwind']
+    },
+]
+
+export default projectData
