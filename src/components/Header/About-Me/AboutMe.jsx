14222efe566b4222995877316a26505b4bd9818f
mohsen <mohsenseyedi1382@gmail.com>
2023-10-11 17:56:07 +0330

    Responsive AboutMe component

--- src/components/Header/About-Me/AboutMe.jsx
+++ src/components/Header/About-Me/AboutMe.jsx
@@ -3,42 +3,42 @@ import React from 'react'
 
 export default function AboutMe () {
     return (
-        <div id="aboutMe-section" className="w-full h-screen flex items-center justify-center mt-60">
-            <div className="w[95%] flex">
-                <div className="w-[45%]">
+        <div id="aboutMe-section" className="w-full h-screen flex items-center justify-center mt-60 md:h-auto">
+            <div className="w[95%] flex md:flex-col-reverse md:items-center">
+                <div className="w-[45%] md:hidden">
                     <img src="./img/me/msg317320486-21967_prev_ui.png" alt="" />
                 </div>
-                <div className="w-[55%] h-auto text-4xl mt-12">
-                    <h4 className="header-text before:content-['About']">About Me</h4>
-                    <p className="text-gray-color text-3xl mt-20">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                <div className="w-[55%] h-auto text-4xl mt-12 lg:text-3xl md:w-[95%] md:text-4xl md:pl-32 sm:pl-4 sm:text-3xl vs:text-2xl">
+                    <h4 className="header-text before:content-['About'] ">About Me</h4>
+                    <p className="text-gray-color text-3xl mt-20 lg:w-[80%] md:w-[90%] sm:w-full">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
                     <ul className="w-full mt-14">
-                        <li className="w-full flex mt-4">
+                        <li className="w-full flex mt-4 md:mt-10">
                             <span className="w-[40%] font-bold">Name:</span>
-                            <span className="w-[40%] text-gray-color">Mohsen Seyedi</span>
+                            <span className="w-[40%] text-gray-color sm:ml-6 vs:ml-12">Mohsen Seyedi</span>
                         </li>
-                        <li className="w-full flex mt-4">
+                        <li className="w-full flex mt-4 md:mt-10">
                             <span className="w-[40%] font-bold">Date of birth:</span>
-                            <span className="w-[40%] text-gray-color">January 01, 1987</span>
+                            <span className="w-[40%] text-gray-color sm:ml-6 vs:ml-12">January 01, 1987</span>
                         </li>
-                        <li className="w-full flex mt-4">
+                        <li className="w-full flex mt-4 md:mt-10">
                             <span className="w-[40%] font-bold">Address:</span>
-                            <span className="w-[40%] text-gray-color">Iran-Tabriz</span>
+                            <span className="w-[40%] text-gray-color sm:ml-6 vs:ml-12">Iran-Tabriz</span>
                         </li>
-                        <li className="w-full flex mt-4">
+                        <li className="w-full flex mt-4 md:mt-10">
                             <span className="w-[40%] font-bold">Zip code:</span>
-                            <span className="w-[40%] text-gray-color">1000</span>
+                            <span className="w-[40%] text-gray-color sm:ml-6 vs:ml-12">1000</span>
                         </li>
-                        <li className="w-full flex mt-4">
+                        <li className="w-full flex mt-4 md:mt-10">
                             <span className="w-[40%] font-bold">Email:</span>
-                            <span className="w-[40%] text-gray-color">mohsenseyedi@gmail.com</span>
+                            <span className="w-[40%] text-gray-color sm:ml-6 vs:ml-12">mohsenseyedi@gmail.com</span>
                         </li>
-                        <li className="w-full flex mt-4">
+                        <li className="w-full flex mt-4 md:mt-10">
                             <span className="w-[40%] font-bold">Phone:</span>
-                            <span className="w-[40%] text-gray-color">09305859071</span>
+                            <span className="w-[40%] text-gray-color sm:ml-6 vs:ml-12">09305859071</span>
                         </li>
                     </ul>
                     <h4 className="opacity-80 my-14"><span className="opacity-100 text-yellow-color pr-2 font-sans">20</span>Project complete</h4>
-                    <a href="#" className="btn text-[1.4rem] py-5 px-12">Download cv</a>
+
                 </div>
             </div>
         </div>
--- src/components/Header/Header/Header.jsx
+++ src/components/Header/Header/Header.jsx
@@ -10,10 +10,10 @@ export default function Header () {
         <div id="Header" className="w-full h-auto">
             <NavBar></NavBar>
             <div id="section-img" className="w-full h-auto flex items-center relative">
-                <div id="section-img-text" className="w-[40%] pb-24 pl-16 z-50 md:absolute md:w-[80%] md:ml-28 sm:w-full sm:ml-4">
+                <div id="section-img-text" className="w-[40%] pb-24 pl-16 z-50 md:absolute md:w-[80%] md:ml-28 sm:w-full sm:ml-[-2rem]">
                     <p className="text-yellow-color text-4xl">HELLO !</p>
                     <h3 className="text-[6rem] text-yellow-color font-bold"><span className="text-white">I'm</span> Mohsen Seyedi</h3>
-                    <p className="text-5xl text-white mt-6 lg:text-4xl md:text-5xl md:font-bold md:text-black">A FrontEnd Developer</p>
+                    <p className="text-5xl text-white mt-6 lg:text-4xl md:text-5xl md:font-bold">A FrontEnd Developer</p>
                     <div id="header-btns" className="w-full flex items-center text-xl mt-12">
                         <a href="#form-contact" className="font-bold border border-yellow-color bg-yellow-color text-black py-6 px-14 rounded-full">HIRE ME</a>
                     </div>
