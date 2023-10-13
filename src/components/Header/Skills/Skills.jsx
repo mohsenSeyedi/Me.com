4649e2d0a09d39a4fcb5bcae4b6b8d1375b1aa39
mohsen <mohsenseyedi1382@gmail.com>
2023-10-13 17:49:44 +0330

    Responsive skills component

--- src/components/Header/Skills/Skills.jsx
+++ src/components/Header/Skills/Skills.jsx
@@ -11,9 +11,9 @@ export default function Skills() {
     <div className="w-[95%] flex flex-col items-center justify-center">
         <div className="flex flex-col items-center justify-center">
             <h4 className="header-text before:content-['Myskills'] before:left-[-5rem] before:mt-[-2rem]">My skills</h4>
-            <p className="text-3xl text-center mt-14">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+            <p className="text-3xl text-center mt-14 md:w-[85%]">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
         </div>
-        <div className="w-[95%] grid grid-cols-2 gap-12 mt-20">
+        <div className="w-[95%] grid grid-cols-2 gap-12 mt-20 md:w-[80%] sm:w-[95%] md:grid-cols-1">
           {skills.map(skill => (
             <Progress key={skill.id} {...skill}></Progress>
           ))}
