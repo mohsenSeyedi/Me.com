718a01fc84318cc40d9a17584b4d1c2c332acdcf
mohsen <mohsenseyedi1382@gmail.com>
2023-10-16 13:44:35 +0330

    add Aos to Skills component

--- src/components/Header/Skills/Skills.jsx
+++ src/components/Header/Skills/Skills.jsx
@@ -1,10 +1,16 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import Progress from './Progress'
 import skillsData from "./data"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function Skills() {
 
   const [skills , setSkills] = useState(skillsData)
+
+  useEffect(() => {
+    AOS.init();
+  }, [])
   
   return (
     <div id="skills-section" className="w-full mt-40 flex items-center justify-center">
@@ -13,7 +19,7 @@ export default function Skills() {
             <h4 className="header-text before:content-['Myskills'] before:left-[-5rem] before:mt-[-2rem]">My skills</h4>
             <p className="text-3xl text-center mt-14 md:w-[85%]">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
         </div>
-        <div className="w-[95%] grid grid-cols-2 gap-12 mt-20 md:w-[80%] sm:w-[95%] md:grid-cols-1">
+        <div className="w-[95%] grid grid-cols-2 gap-12 mt-20 md:w-[80%] sm:w-[95%] md:grid-cols-1" data-aos="fade-up" data-aos-duration="1000">
           {skills.map(skill => (
             <Progress key={skill.id} {...skill}></Progress>
           ))}
