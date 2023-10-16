b4bc675a0e20d196a48be58d15df6a842f469c36
mohsen <mohsenseyedi1382@gmail.com>
2023-10-16 13:38:39 +0330

    add Aos to About-Me component

--- src/components/Header/About-Me/AboutMe.jsx
+++ src/components/Header/About-Me/AboutMe.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-
+import React,{useEffect} from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function AboutMe () {
+
+    useEffect(() => {
+        AOS.init();
+    }, [])
+
+
     return (
         <div id="aboutMe-section" className="w-full h-screen flex items-center justify-center mt-60 md:h-auto">
             <div className="w[95%] flex md:flex-col-reverse md:items-center">
                 <div className="w-[45%] md:hidden">
-                    <img src="./img/me/msg317320486-21967_prev_ui.png" alt="" />
+                    <img src="./img/me/msg317320486-21967_prev_ui.png" alt="" data-aos="fade-up" data-aos-duration="1000" />
                 </div>
-                <div className="w-[55%] h-auto text-4xl mt-12 lg:text-3xl md:w-[95%] md:text-4xl md:pl-32 sm:pl-4 sm:text-3xl vs:text-2xl">
+                <div className="w-[55%] h-auto text-4xl mt-12 lg:text-3xl md:w-[95%] md:text-4xl md:pl-32 sm:pl-4 sm:text-3xl vs:text-2xl" data-aos="fade-down" data-aos-duration="500">
                     <h4 className="header-text before:content-['About'] ">About Me</h4>
                     <p className="text-gray-color text-3xl mt-20 lg:w-[80%] md:w-[90%] sm:w-full">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
                     <ul className="w-full mt-14">
